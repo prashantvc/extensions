6d7ecbe2ec41f8397ce42c13b80e2646cfd92f9e
Prashant Cholachagudda <pvc@outlook.com>
2023-02-04 13:34:08 +0530

    Added web api code

--- Program.cs
+++ Program.cs
@@ -1,10 +1,41 @@
-﻿
-var app = new CommandApp();
-app.Configure(config =>
+﻿public static class Program
 {
-    config.AddCommand<AddExtensionFileCommand>("add")
-        .WithDescription("Add extension package to the repository");
-    config.AddCommand<ListExtensionsCommand>("list");
-});
+    public static async Task<int> Main(string[] args)
+    {
+        // Run app as web app
+        if (args[0] == "run")
+        {
+            var builder = WebApplication.CreateBuilder(args);
 
-return await app.RunAsync(args);
+            builder.Services.AddControllers();
+            // Add services to the container.
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var webApp = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (webApp.Environment.IsDevelopment())
+            {
+                webApp.UseSwagger();
+                webApp.UseSwaggerUI();
+            }
+            webApp.UseHttpsRedirection();
+            webApp.UseAuthorization();
+            webApp.MapControllers();
+
+            await webApp.RunAsync();
+            return 0;
+        }
+
+        var app = new CommandApp();
+        app.Configure(config =>
+        {
+            config.AddCommand<AddExtensionFileCommand>("add")
+                .WithDescription("Add extension package to the repository");
+            config.AddCommand<ListExtensionsCommand>("list");
+        });
+        return await app.RunAsync(args);
+    }
+}
