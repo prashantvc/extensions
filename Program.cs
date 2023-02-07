3334427f94be500a73d5cb18080bdf25163a9cdf
Prashant Cholachagudda <pvc@outlook.com>
2023-02-07 21:36:20 +0530

    making it into a web app

--- Program.cs
+++ Program.cs
@@ -1,40 +1,21 @@
-﻿public static class Program
+﻿
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<ILiteDbContext, LiteDbContext>();
+builder.Services.AddControllers();
+// Add services to the container.
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var webApp = builder.Build();
+// Configure the HTTP request pipeline.
+if (webApp.Environment.IsDevelopment())
 {
-    public static async Task<int> Main(string[] args)
-    {
-        // Run app as web app
-        if (args.Length > 1 && args[0] == "run")
-        {
-            var builder = WebApplication.CreateBuilder(args);
-
-            builder.Services.AddSingleton<ILiteDbContext, LiteDbContext>();
-            builder.Services.AddControllers();
-            // Add services to the container.
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            var webApp = builder.Build();
-            // Configure the HTTP request pipeline.
-            if (webApp.Environment.IsDevelopment())
-            {
-                webApp.UseSwagger();
-                webApp.UseSwaggerUI();
-            }
-            webApp.UseHttpsRedirection();
-            webApp.UseAuthorization();
-            webApp.MapControllers();
-
-            await webApp.RunAsync();
-            return 0;
-        }
-
-        var app = new CommandApp();
-        app.Configure(config =>
-        {
-            config.AddCommand<AddExtensionFileCommand>("add")
-                .WithDescription("Add extension package to the repository");
-            config.AddCommand<ListExtensionsCommand>("list");
-        });
-        return await app.RunAsync(args);
-    }
+    webApp.UseSwagger();
+    webApp.UseSwaggerUI();
 }
+webApp.UseHttpsRedirection();
+webApp.UseAuthorization();
+webApp.MapControllers();
+
+await webApp.RunAsync();
