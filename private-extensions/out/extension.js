cd8487ff10c8e8082057069d913cbe117be21efe
Prashant Cholachagudda <prchol@microsoft.com>
2023-02-13 17:31:58 +0530

    add the source command

--- private-extensions/out/extension.js
+++ private-extensions/out/extension.js
@@ -7,18 +7,21 @@ const vscode = require("vscode");
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 function activate(context) {
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "private-extensions" is now active!');
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('private-extensions.helloWorld', () => {
-        // The code you place here will be executed every time your command is executed
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World from private-extensions!');
+    let addSource = vscode.commands.registerCommand("private-extensions.addSource", async () => {
+        vscode.window
+            .showInputBox({
+            placeHolder: "Enter the URL of the source",
+        })
+            .then(async (url) => {
+            if (url) {
+                vscode.window.showInformationMessage(`Added source ${url}`);
+                await vscode.workspace
+                    .getConfiguration("")
+                    .update("privateExtensions.Source", [url], vscode.ConfigurationTarget.Global);
+            }
+        });
     });
-    context.subscriptions.push(disposable);
+    context.subscriptions.push(addSource);
 }
 exports.activate = activate;
 // This method is called when your extension is deactivated
