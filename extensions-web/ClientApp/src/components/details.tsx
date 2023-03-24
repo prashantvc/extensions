3dc49a2779780b40a8de0bd0b6684b40df389f47
Prashant Cholachagudda <pvc@outlook.com>
2023-03-24 13:19:47 +0530

    refactor to use async await

--- extensions-web/ClientApp/src/components/details.tsx
+++ extensions-web/ClientApp/src/components/details.tsx
@@ -1,19 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import ReactMarkdown from 'react-markdown';
 import { useParams } from 'react-router-dom';
-import remarkGfm from 'remark-gfm'
+import remarkGfm from 'remark-gfm';
 import { IPackage, PackageWrapper } from '../data/package';
 
 const DetailsPage = () => {
   const { identifier, version } = useParams<{ identifier: string, version: string }>();
   const [readme, setReadme] = useState('');
+
   useEffect(() => {
-    getExtensionDetails(identifier, version).then(data => {
-      fetch(`${data?.extensionPath}/extension/Readme.md`)
-        .then(response => response.text())
-        .then(text => setReadme(text));
-    });
-  }, []);
+    const getReadme = async () => {
+      const packageData = await getExtensionDetails(identifier, version);
+      const readmeResponse = await fetch(`${packageData?.extensionPath}/extension/Readme.md`);
+      const readmeText = await readmeResponse.text();
+      setReadme(readmeText);
+    };
+    getReadme();
+  }, [identifier, version]);
 
   return (
     <div style={{ margin: '24px' }}>
@@ -22,8 +25,7 @@ const DetailsPage = () => {
   );
 };
 
-async function getExtensionDetails(identifier: string | undefined,
-  version: string | undefined) {
+async function getExtensionDetails(identifier: string | undefined, version: string | undefined) {
   const response = await fetch(`extension/${identifier}/${version}`);
   const data: IPackage = await response.json();
   return new PackageWrapper(data);
