1126ac0ca6bc86530a8936b7999926a4c50e88a5
Prashant Cholachagudda <pvc@outlook.com>
2023-03-24 20:35:47 +0530

    added the unit test project

---
+++ extensions-web-tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace extensions_web_tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
