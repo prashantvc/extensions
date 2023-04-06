e0f2093647b6fbb8c212ff00cfe4abada70b52b4
Prashant Cholachagudda <pvc@outlook.com>
2023-04-06 15:30:59 +0530

    refactor code

--- private-extensions/src/utlis.ts
+++ private-extensions/src/utlis.ts
@@ -5,6 +5,12 @@ import * as path from "path";
 
 import { ExtensionPackage } from "./extensionPackage";
 
+/**
+ * Downloads an extension package from the specified source and saves it to the download directory.
+ * @param item The extension package to download.
+ * @param ctx The extension context.
+ * @returns A promise that resolves to true if the download is successful, or false if there's an error.
+ */
 export async function installExtension(
   item: ExtensionPackage,
   ctx: vscode.ExtensionContext
@@ -18,6 +24,7 @@ export async function installExtension(
   if (!fs.existsSync(downloadDirectory)) {
     fs.mkdirSync(downloadDirectory, { recursive: true });
   }
+
   const response = await axios.get(downloadUrl, { responseType: "stream" });
   const fileName = `${item.identifier}-${item.version}.vsix`;
   const extensionPath = path.join(downloadDirectory, fileName);
@@ -25,31 +32,40 @@ export async function installExtension(
   const writer = fs.createWriteStream(extensionPath);
   response.data.pipe(writer);
 
-  return new Promise<boolean>((resolve, reject) => {
-    writer.on("finish", () => {
-      console.log(`Download complete: ${extensionPath}`);
-      vscode.commands
-        .executeCommand(
-          "workbench.extensions.installExtension",
-          vscode.Uri.file(extensionPath)
-        )
-        .then(() => {
-          console.log(`Installed ${item.displayName}`);
-          fs.rmSync(extensionPath);
-          resolve(true);
-        });
-    });
-    writer.on("error", (error) => {
-      console.error(error);
-      resolve(false);
-    });
+  await new Promise<void>((resolve, reject) => {
+    writer.on("finish", resolve);
+    writer.on("error", reject);
   });
+
+  console.log(`Download complete: ${extensionPath}`);
+
+  try {
+    await vscode.commands.executeCommand(
+      "workbench.extensions.installExtension",
+      vscode.Uri.file(extensionPath)
+    );
+    console.log(`Installed ${item.displayName}`);
+    fs.rmSync(extensionPath);
+    return true;
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
 }
 
+/**
+ * Gets the download directory for the extension.
+ * @param ctx The extension context.
+ * @returns A URI representing the download directory.
+ */
 function getDownloadDirectory(ctx: vscode.ExtensionContext): vscode.Uri {
   return vscode.Uri.joinPath(ctx.globalStorageUri, "temp");
 }
 
+/**
+ * Gets the source URL for private extensions.
+ * @returns The source URL for private extensions.
+ */
 export function getExtensionSource(): string {
   let url = vscode.workspace
     .getConfiguration("")
@@ -58,6 +74,10 @@ export function getExtensionSource(): string {
   return url ? url[0] : "";
 }
 
+/**
+ * Gets whether to include prerelease versions of private extensions.
+ * @returns True if prerelease versions should be included, false otherwise.
+ */
 export function getPrerelease(): boolean {
   return (
     vscode.workspace
