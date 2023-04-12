fbe288e633694ff1c82844209a24b279bbc8b6ed
Prashant Cholachagudda <pvc@outlook.com>
2023-04-12 19:23:25 +0530

    refactor version group method

--- extensions-cli/src/showExtension.ts
+++ extensions-cli/src/showExtension.ts
@@ -1,12 +1,5 @@
-import { ExtensionVersion } from "azure-devops-node-api/interfaces/GalleryInterfaces";
-import { getPublicGalleryAPIUrl } from "./util";
-import * as semver from "semver";
+import { getPublicGalleryAPIUrl, versionGroup } from "./util";
 import { VSCodePublishedExtension } from "./galleryapi";
-
-export interface ExtensionVersionGroup {
-	readonly version: semver.SemVer;
-	readonly details: { version: string; lastUpdated: Date; targetPlatform: string | undefined }[];
-}
 
 export async function showExtension(identifier: string) {
 	const extension = await getPublicGalleryAPIUrl().getExtensionById(identifier);
@@ -18,7 +11,6 @@ export async function showExtension(identifier: string) {
 }
 
 function printExtensionDetails(ext: VSCodePublishedExtension) {
-	const vrs = groupVersionsByVersionString(ext.versions!);
 	console.log(`${ext.displayName} - ${ext.publisher.publisherName}.${ext.extensionName}`);
 	console.log(`${ext.shortDescription}`);
 	console.log(`\nPublisher: ${ext.publisher.displayName}`);
@@ -27,34 +19,6 @@ function printExtensionDetails(ext: VSCodePublishedExtension) {
 	);
 
 	//print latest version
+	const vrs = versionGroup(ext.versions!);
 	console.log(`Latest version: ${vrs[0].version}`);
-}
-
-function groupVersionsByVersionString(versionList: ExtensionVersion[], maxVersions = 5) {
-	const versionsByNumber = versionList.reduce((acc, version) => {
-		const versionString = version.version!;
-		if (!acc[versionString]) {
-			acc[versionString] = [];
-		}
-		acc[versionString].push({
-			version: version.version!,
-			lastUpdated: version.lastUpdated!,
-			targetPlatform: version.targetPlatform,
-		});
-		return acc;
-	}, {} as { [version: string]: { version: string; lastUpdated: Date; targetPlatform: string | undefined }[] });
-
-	const versionGroup = Object.keys(versionsByNumber)
-		.map((v) => {
-			return {
-				version: new semver.SemVer(v),
-				details: versionsByNumber[v],
-			} as ExtensionVersionGroup;
-		})
-		.sort((a, b) => {
-			return semver.compare(b.version!, a.version!);
-		})
-		.slice(0, maxVersions);
-
-	return versionGroup;
 }
--- extensions-cli/src/util.ts
+++ extensions-cli/src/util.ts
@@ -1,7 +1,43 @@
 import { GalleryApi } from "./galleryapi";
+import { ExtensionVersion } from "azure-devops-node-api/interfaces/GalleryInterfaces";
+import * as semver from "semver";
 
 const marketplaceUrl = "https://marketplace.visualstudio.com/_apis/public/gallery";
 
 export function getPublicGalleryAPIUrl() {
 	return new GalleryApi(marketplaceUrl, "3.0-preview.1");
+}
+
+export function versionGroup(versionList: ExtensionVersion[], maxVersions = 5): ExtensionVersionGroup[] {
+	const versionsByNumber = versionList.reduce((acc, version) => {
+		const versionString = version.version!;
+		if (!acc[versionString]) {
+			acc[versionString] = [];
+		}
+		acc[versionString].push({
+			version: version.version!,
+			lastUpdated: version.lastUpdated!,
+			targetPlatform: version.targetPlatform,
+		});
+		return acc;
+	}, {} as { [version: string]: { version: string; lastUpdated: Date; targetPlatform: string | undefined }[] });
+
+	const versionGroup = Object.keys(versionsByNumber)
+		.map((v) => {
+			return {
+				version: new semver.SemVer(v),
+				details: versionsByNumber[v],
+			} as ExtensionVersionGroup;
+		})
+		.sort((a, b) => {
+			return semver.compare(b.version!, a.version!);
+		})
+		.slice(0, maxVersions);
+
+	return versionGroup;
+}
+
+export interface ExtensionVersionGroup {
+	readonly version: semver.SemVer;
+	readonly details: { version: string; lastUpdated: Date; targetPlatform: string | undefined }[];
 }
